2c844f0d1be6c636ee56d5daa309c24d7743ca49
ericlau00 <ericlau41802@gmail.com>
2020-11-21 13:45:23 -0500

    Add pie charts on map

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 window.onload = async () => {
     let data = await d3.csv('NYPD_Hate_Crimes.csv');
 
+    let lgbtMotives = [
+        "ANTI-MALE HOMOSEXUAL(GAY)",
+        "ANTI-FEMALE HOMOSEXUAL(GAY)",
+        "ANTI-TRANSGENDER",
+        "ANTI-LGBT(MIXED GROUP)"
+    ];
+
+    for(let i = 0; i < data.length; i++) {
+        if (lgbtMotives.includes(data[i]["Bias Motive Description"])) {
+            data[i]["Bias Motive Description"] = "ANTI-LGBTQ+";
+        }
+    }
+
     // let categories = d3.group(data, d => d['Offense Category']);
     // let motives = d3.group(data, d => d['Bias Motive Description']);
     // let cat = d3.rollup(data, v => v.length, d => d['Offense Category']);
 
     let mot = d3.rollup(data, v => v.length, d => d['Bias Motive Description']);
-
-    let homo = mot.get("ANTI-MALE HOMOSEXUAL(GAY)") 
-            + mot.get("ANTI-FEMALE HOMOSEXUAL(GAY)")
-            + mot.get("ANTI-TRANSGENDER")
-            + mot.get("ANTI-LGBT(MIXED GROUP)");
-    mot.delete("ANTI-MALE HOMOSEXUAL(GAY)");
-    mot.delete("ANTI-FEMALE HOMOSEXUAL(GAY)");
-    mot.delete("ANTI-TRANSGENDER");
-    mot.delete("ANTI-LGBT(MIXED GROUP)");
-
-    mot.set("ANTI-LGBTQ+", homo);
 
     let other = 0;
     let otherMotives = Array();
@@ -119,5 +121,104 @@ window.onload = async () => {
         .attr('stroke-linejoin', 'round')
         .attr('stroke-width', 1.5)
         .attr('d', path);
-            
+
+    let getBoroughData = (boroughName) => {
+        let boroData = data.filter(d => d["County"] === boroughName);
+        let boroMotives = d3.rollup(boroData, v => v.length, d => d['Bias Motive Description']);
+        let o = 0;
+        let total = 0;
+
+        boroMotives.forEach((v, k) => {
+            total += v;
+            if (v < 3 || k === "") {
+                o += v;
+                boroMotives.delete(k);
+            }
+        })
+
+        let s = [...boroMotives.entries()].sort((a, b) => b[1] - a[1]);
+
+        s.push(["OTHER", o]);
+
+        for (let i = 0; i < s.length; i++) {
+            s[i] = {
+                name: s[i][0],
+                number: s[i][1],
+                percent: s[i][1] / total * 100
+            }
+        }
+        
+        return { data: s, total: total };
+    }
+
+    let si = getBoroughData("RICHMOND");
+    let bk = getBoroughData("KINGS");
+    let qn = getBoroughData("QUEENS");
+    let mn = getBoroughData("NEW YORK");
+    let bx = getBoroughData("BRONX");
+
+    console.log(si, bk, qn, mn, bx);
+
+    let mapDomain = [
+        'ANTI-JEWISH',
+        'ANTI-LGBTQ+',
+        'ANTI-BLACK',
+        'ANTI-WHITE',
+        'ANTI-ASIAN',
+        'ANTI-ISLAMIC(MUSLIM)',
+        'ANTI-CATHOLIC',
+        'ANTI-OTHER ETHNICITY',
+        'OTHER',
+        'ANTI-ARAB',
+        'ANTI-HISPANIC',
+        'ANTI-MULTI RACIAL GROUPS'
+    ];
+
+    let mapColorScale = d3.scaleOrdinal()
+        .domain(mapDomain)
+        .range(d3.schemeSet3);
+
+    let pie = d3.pie()
+        .padAngle(0.005)
+        .value(d => d.percent);
+
+    let drawPie = (boro, x, y) => {
+        let arcs = pie(boro.data);
+        let radius = (mapWidth * boro.total / total) * 0.5;
+        let arc = d3.arc()
+            .innerRadius(radius * 0.6)
+            .outerRadius(radius - 1);
+
+        map.selectAll('.pie')
+            .data(arcs)
+            .enter().append('path')
+            .attr('fill', d => mapColorScale(d.data.name))
+            .attr('d', arc)
+            .attr('transform', `translate(${mapWidth / x}, ${mapWidth / y})`)
+    }
+
+    drawPie(bk, 1.85, 1.5);
+    drawPie(si, 5, 1.25);
+    drawPie(mn, 1.9, 4);
+    drawPie(bx, 1.4, 7);
+    drawPie(qn, 1.2, 2);
+
+    map.selectAll('.label')
+        .data(mapDomain)
+        .enter().append('rect')
+        .attr('x', mapWidth / 20)
+        .attr('y', (d, i) => i * (mapWidth / 45) + (mapWidth / 10))
+        .attr('width', mapWidth / 50)
+        .attr('height', mapWidth / 50)
+        .attr('fill', d => mapColorScale(d));
+
+    map.selectAll('.labelText')
+        .data(mapDomain)
+        .enter().append('text')
+        .attr('x', mapWidth / 12.5)
+        .attr('y', (d, i) => i * (mapWidth / 45) + (mapWidth / 8.5))
+        .attr('font-size', mapWidth / 50)
+        .attr('font-family', 'sans-serif')
+        .attr('font-weight', 'bold')
+        .text(d => d)
 }
